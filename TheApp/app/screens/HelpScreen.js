ee2147fdb0e676c29bcab2f57b0746f81d26bb2d
eyalwa94 <80194472+eyalwa94@users.noreply.github.com>
2021-05-08 19:33:40 +0300

    help screen works added new buttons to admin

--- TheApp/app/screens/HelpScreen.js
+++ TheApp/app/screens/HelpScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text, ActivityIndicator ,Linking } from "react-native";
+import { View, Text, ActivityIndicator ,Linking ,StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 import { firestore } from "../api/firebase";
 
 
 const HelpScreen = ({ route, navigation }) => {
-  const { help } = route.params;
+  const  {all_volunteers}  = route.params;
+  const bg_colors=["red","green","blue","yellow","white","purple"];
+
+
+
+  /*
   const [Volunteers, setVolunteers] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
 
@@ -32,9 +38,9 @@ const HelpScreen = ({ route, navigation }) => {
       </View>
     );
   }
-  
+  */
   return (
-    <View style={{ flex: 1, justifyContesnt: "center", alignItems: "center" }}>
+   /* <View style={{ flex: 1, justifyContesnt: "center", alignItems: "center" }}>
       <Text>מה שנבחר זה {help}</Text>
       <Text onPress={() => {
             Linking.openURL(
@@ -44,8 +50,35 @@ const HelpScreen = ({ route, navigation }) => {
       {Volunteers.map((item, key) => {
         return <Text key={key}>{item.firstName}</Text>;
       })}
+    </View>*/
+    <View style={{ flex: 1, justifyContesnt: "center", alignItems: "center" }}>
+      <Text style={{ fontWeight: 'bold' , fontSize:30 }}>{all_volunteers[0].helpType}</Text>
+      {all_volunteers.map((item, key) => {
+        return (<View key={key} style={{flex: 1 , backgroundColor: bg_colors[Math.floor(Math.random()*bg_colors.length)] , width: "100%" ,  }}>
+          <Text style={styles.text}>שם: {item.firstName}</Text>
+          <Text style={styles.text} >שם משפחה: {item.lastName}</Text>
+          <Text style={styles.text}>עיר: {item.city}</Text>
+          <Text style={styles.text}>מספר טלפון: {item.phone}</Text>
+          <Button onPress={() => Linking.openURL(item.calendlyLink)}>link for calendly</Button>
+          </View>);
+      })}  
     </View>
   );
 };
+//https://calendly.com/ziv-birer/rabbi
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFEBCD",
+  },
+  space: {
+    width: 20, // or whatever size you need
+    height: 20,
+  },
+  text:{
+    textAlign: "right",
+    writingDirection: "rtl",
+  }
+});
 
 export default HelpScreen;
